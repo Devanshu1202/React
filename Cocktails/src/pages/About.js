26c1065af4fbbf3879fc6c9ca77294b1d24cf6c5
devanshu1202 <devanshusingh1202@gmail.com>
2021-07-12 23:41:22 +0530

    Using Api Project

---
+++ Cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        lorem dfjkn dsfjdflkj sdffjelj fdsljdfljfds ldsfjlsdjf pfdsfd;ldsf pf fdk;fdsk;lfdsk ;fdk
+      </p>
+    </section>
+  )
+}
+
+export default About
