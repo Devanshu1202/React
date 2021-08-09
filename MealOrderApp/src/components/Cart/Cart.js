b49b6f9abad288bcb7ecbd1abd739f1510b6c00f
devanshu1202 <devanshusingh1202@gmail.com>
2021-08-09 23:32:18 +0530

    Using forebase

--- MealOrderApp/src/components/Cart/Cart.js
+++ MealOrderApp/src/components/Cart/Cart.js
@@ -1,10 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
+
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+
+  const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
 
   const hasItems = cartCtx.items.length > 0;
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -30,20 +36,63 @@ const Cart = (props) => {
       ))}
     </ul>
   );
-  return (
-    <Modal onClose={props.onCloseClick}>
+
+  const orderHandler = () => {
+    setIsCheckout(true);
+  };
+
+  const submitOrderHandler = (userData) => {
+    setIsSubmitting(true);
+    fetch("https://react-http-5c965-default-rtdb.firebaseio.com/orders.json", {
+      method: "POST",
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    });
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+  };
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button-alt"]} onClick={props.onCloseClick}>
+        Close
+      </button>
+
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  );
+
+  const cartModalContent = (
+    <React.Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button-alt"]} onClick={props.onCloseClick}>
-          Close
-        </button>
+      {isCheckout && (
+        <Checkout
+          onConfirm={submitOrderHandler}
+          onCancel={props.onCloseClick}
+        />
+      )}
+      {!isCheckout && modalActions}
+    </React.Fragment>
+  );
+  const isSubmittingModalContent = <p>Sending order data.....</p>;
+  const didSubmitModalContent = <p>Successfully sent the order</p>;
 
-        {hasItems && <button className={classes.button}>Order</button>}
-      </div>
+  return (
+    <Modal onClose={props.onCloseClick}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- MealOrderApp/src/components/Meals/AvailableMeals.js
+++ MealOrderApp/src/components/Meals/AvailableMeals.js
@@ -5,11 +5,18 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null);
+
   useEffect(() => {
+    setIsLoading(true);
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-http-5c965-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -22,10 +29,32 @@ const AvailableMeals = () => {
           price: responseData[key].price,
         });
       }
+      setIsLoading(false);
       setMeals(loadedMeals);
     };
-    fetchMeals();
+    try {
+      fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+    } catch (error) {}
   }, []);
+
+  if (loading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>Failed to fetch Data</p>
+      </section>
+    );
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
@@ -36,11 +65,11 @@ const AvailableMeals = () => {
     />
   ));
   return (
-    <Card>
-      <section className={classes.meals}>
+    <section className={classes.meals}>
+      <Card>
         <ul>{mealsList}</ul>
-      </section>
-    </Card>
+      </Card>
+    </section>
   );
 };
 
